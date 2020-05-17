40208637992a62eda3d0e68d96ba0ce8c4bb132a
mwiszenko <wiszenkom@gmail.com>
2020-05-17 16:46:00 +0200

    Declared classes

---
+++ src/com/mwiszenko/battleship/Game.java
@@ -0,0 +1,7 @@
+package com.mwiszenko.battleship;
+
+public class Game {
+    public static void main( String[] args) {
+
+    }
+}
---
+++ src/com/mwiszenko/battleship/Ship.java
@@ -0,0 +1,4 @@
+package com.mwiszenko.battleship;
+
+public class Ship {
+}
